cf4cfb32d7b05f11adbd5763004f1b59377fdfde
Ourania <ourania.argyra@eurodyn.com>
2026-02-15 15:19:23 +0200

    feat(master) - Updated database by adding new props in models.

---
+++ SpanishClass/Migrations/20260215122016_AddedExtraPropertiesInModels.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpanishClass.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedExtraPropertiesInModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LessonName",
+                table: "Lessons",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LessonName",
+                table: "Lessons");
+        }
+    }
+}
